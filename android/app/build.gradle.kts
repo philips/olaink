bbe21ccf3fb3cc9a82ce9bb0eee1026a516521ac
Brandon Philips's Clanker <brandon+clanker@ifup.org>
2026-08-25 15:55:47 -0700

    center companion onboarding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     applicationId = "dev.olaink.player"
     minSdk = 23
     targetSdk = 35
-    versionCode = 1
-    versionName = "0.0.1"
+    versionCode = 2
+    versionName = "0.0.2"
   }
 }
 
